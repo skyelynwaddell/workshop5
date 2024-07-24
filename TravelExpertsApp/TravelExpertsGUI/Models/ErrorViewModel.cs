117a82aeb7fb4d870ea581185d4f4da7124acca8
Skye <dillonleewaddell@gmail.com>
2024-07-24 15:53:10 -0600

    first commit

---
+++ TravelExpertsApp/TravelExpertsGUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelExpertsGUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
